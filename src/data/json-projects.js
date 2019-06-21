4ecea9f3300a278437a6314ef0dcb04524eb6dcf
Andreas Kochmann <andreaskochmann85@gmail.com>
2019-06-21 08:33:14 +0200

    added json data template

---
+++ src/data/json-projects.js
@@ -0,0 +1,9 @@
+export const projects = [
+    {
+        projectName : 'name',
+        url         : '#',
+        description : 'blabla',
+        firstUpload : 'xx-xx-xxxx',
+        lastUpdate  : 'xx-xx-xxxx'
+    }
+];
