02123409d1c6282b24d158da4a88268f3f57247e
Andreas Kochmann <andreaskochmann85@gmail.com>
2019-06-21 08:11:22 +0200

    added projects.js

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import Header from './header';
+import Projects from './projects';
 
 const App = () => {
     return (
         <div>
-            <Header />
+            <Header     />
+            <Projects   />
         </div>
     )
 };
