b9432e970532ca18510ca4131db932e710f818b3
Andreas Kochmann <andreaskochmann85@gmail.com>
2019-06-21 19:52:17 +0200

    added styling/mapped through further details

--- src/components/projects.js
+++ src/components/projects.js
@@ -6,7 +6,17 @@ const Projects = () => {
     const createdProjects = projects.map((proj) => {
         return (
             <div key={proj.projectnumber}>
-                { proj.projectName }
+                <h2>{ proj.projectName }</h2> <hr />
+                Url  :  <a href={ proj.url } target='_blank' rel='noopener noreferrer' >{ proj.url }</a>
+                <br/><br/>
+                { proj.description }
+                <br/><br/>
+                { proj.usedTechnics }
+                <br/><br/>
+                First Deployment : { proj.firstUpload }
+                <br/><br/>
+                Last Update : { proj.lastUpdate }
+                <br/><br/><br/>
             </div>
         )
     });
--- src/data/json-projects.js
+++ src/data/json-projects.js
@@ -2,30 +2,30 @@ export const projects = [
     {
         projectnumber   : '1',
         projectName     : 'swapiApi',
-        url             : '#',
-        description     : 'blabla',
+        url             : 'https://crazysession.github.io/swapiapi/',
+        description     : 'Just displaying some StarWars Facts with help of an api',
         screenshots     : ['one','two'],
-        usedTechnics    : ['HTML','CSS','Javascript','React'],
-        firstUpload     : 'xx-xx-xxxx',
-        lastUpdate      : 'xx-xx-xxxx'
+        usedTechnics    : ['HTML ','CSS ','Javascript ','React '],
+        firstUpload     : '05-18-2019',
+        lastUpdate      : '05-18-2019'
     },
     {
         projectnumber   : '2',
         projectName     : 'guessthenumber',
-        url             : '#',
-        description     : 'blabla',
+        url             : 'https://crazysession.github.io/guessthenumber/',
+        description     : 'Little guess the number game with some random Fonts and Victory Pics',
         screenshots     : ['one','two'],
-        usedTechnics    : ['HTML','CSS','Javascript','React'],
-        firstUpload     : 'xx-xx-xxxx',
-        lastUpdate      : 'xx-xx-xxxx'
+        usedTechnics    : ['HTML ','CSS ','Javascript ','React ','Redux '],
+        firstUpload     : '06-19-2019',
+        lastUpdate      : '06-19-2019'
     },
     {
         projectnumber   : '3',
         projectName     : 'project-timeline',
         url             : '#',
-        description     : 'blabla',
+        description     : 'Timeline for all my created and deployed Projects',
         screenshots     : ['one','two'],
-        usedTechnics    : ['HTML','CSS','Javascript','React'],
+        usedTechnics    : ['HTML ','CSS ','Javascript ','React '],
         firstUpload     : 'xx-xx-xxxx',
         lastUpdate      : 'xx-xx-xxxx'
     }
