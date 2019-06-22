4aad120bb82abc5bf737abbecd43cedd3e081ed2
Andreas Kochmann <andreaskochmann85@gmail.com>
2019-06-22 11:46:20 +0200

    added images - changed order of cards

--- src/components/projects.js
+++ src/components/projects.js
@@ -5,11 +5,14 @@ import { projects } from '../data/json-projects';
 const Projects = () => {
     const createdProjects = projects.map((proj) => {
         return (
-            <div key={proj.projectnumber}>
+            <div key={proj.projectnumber} style={ proj.projectnumber % 2 ? {marginLeft: '20%' }:{marginLeft:'60%'}}>
                 <div className="card mb-3" style={{ maxWidth: '540px' }}>
                     <div className="row no-gutters">
                         <div className="col-md-4">
-                            <img src="#" className="card-img" alt="..." />
+                            <img src={proj.screenshots[0]}
+                                 className="card-img"
+                                 style={{width:'100%', height:'60%'}}
+                                 alt="..." />
                         </div>
                         <div className="col-md-8">
                             <div className="card-body">
--- src/data/json-projects.js
+++ src/data/json-projects.js
@@ -1,33 +1,37 @@
+import projectTimeline from '../img/project-timeline.jpg';
+import guessthenumber from '../img/guessthenumber.jpg';
+import swapiapi from '../img/swapiapi.jpg';
+
 export const projects = [
     {
-        projectnumber   : '1',
+        projectnumber   : 1,
         projectName     : 'swapiApi',
         url             : 'https://crazysession.github.io/swapiapi/',
         sourceURL       : 'https://github.com/CrazySession/swapiapi',
         description     : 'Just displaying some StarWars Facts with help of an api',
-        screenshots     : ['one','two'],
+        screenshots     : [ swapiapi ,'two'],
         usedTechnics    : ['HTML ','CSS ','Javascript ','React '],
         firstUpload     : '05-18-2019',
         lastUpdate      : '05-18-2019'
     },
     {
-        projectnumber   : '2',
+        projectnumber   : 2,
         projectName     : 'guessthenumber',
         url             : 'https://crazysession.github.io/guessthenumber/',
         sourceURL       : 'https://github.com/CrazySession/guessthenumber',
         description     : 'Little guess the number game with some random Fonts and Victory Pics',
-        screenshots     : ['one','two'],
+        screenshots     : [ guessthenumber ,'two'],
         usedTechnics    : ['HTML ','CSS ','Javascript ','React ','Redux '],
         firstUpload     : '06-19-2019',
         lastUpdate      : '06-19-2019'
     },
     {
-        projectnumber   : '3',
+        projectnumber   : 3,
         projectName     : 'project-timeline',
         url             : '#',
         sourceURL       : 'https://github.com/CrazySession/project-timeline',
         description     : 'Timeline for all my created and deployed Projects',
-        screenshots     : ['one','two'],
+        screenshots     : [ projectTimeline ,'two'],
         usedTechnics    : ['HTML ','CSS ','Bootstrap ','Javascript ','React '],
         firstUpload     : 'xx-xx-xxxx',
         lastUpdate      : 'xx-xx-xxxx'
